1089bb5313fa56bcfe0398e74b86aa45c433ca24
AleXx815 <davgoga815@gmail.com>
2023-07-11 15:57:41 -0500

    button functionality

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -17,6 +17,10 @@ class CubitCounterScreen extends StatelessWidget {
 class _CubitCounterView extends StatelessWidget {
   const _CubitCounterView();
 
+  void increaseCounterBy(BuildContext context, [int value = 1]) {
+    context.read<CounterCubit>().increaseBy(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     final counterState = context.watch<CounterCubit>().state;
@@ -27,7 +31,9 @@ class _CubitCounterView extends StatelessWidget {
         title: Text("Cubit Counter: ${counterState.transactionCount}"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<CounterCubit>().reset();
+            },
             icon: const Icon(Icons.refresh_outlined),
           ),
         ],
@@ -46,7 +52,7 @@ class _CubitCounterView extends StatelessWidget {
           FloatingActionButton(
             heroTag: "1",
             child: const Text("+3"),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 3),
           ),
           const SizedBox(
             height: 15,
@@ -54,7 +60,7 @@ class _CubitCounterView extends StatelessWidget {
           FloatingActionButton(
             heroTag: "2",
             child: const Text("+2"),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 2),
           ),
           const SizedBox(
             height: 15,
@@ -62,7 +68,7 @@ class _CubitCounterView extends StatelessWidget {
           FloatingActionButton(
             heroTag: "3",
             child: const Text("+1"),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context, 1),
           ),
         ],
       ),
